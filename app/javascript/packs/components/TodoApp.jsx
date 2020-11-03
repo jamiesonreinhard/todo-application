b93d2e8dd245b144f315fc55495443b3415ff670
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-11-03 15:37:35 -0700

    brain spinning

---
+++ app/javascript/packs/components/TodoApp.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from "axios";
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItems: []
+    };
+    this.getTodoItems = this.getTodoItems.bind(this);
+  }
+  componentDidMount() {
+    this.getTodoItems();
+  }
+  getTodoItems() {
+    axios
+      .get("/api/v1/todo_items")
+      .then(response => {
+        const todoItems = response.data;
+        this.setState({ todoItems });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <p>TodoApp</p>
+    )
+  }
+}
+
+document.addEventListener('turbolinks:load', () => {
+  const app = document.getElementById('todo-app')
+  app && ReactDOM.render(<TodoApp />, app)
+})
